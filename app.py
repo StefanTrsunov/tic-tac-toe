8d61ee988f38c84919304e1e33df1bc354ede83a
StefanTrsunov <trsunovstefan@gmail>
2022-02-20 09:37:03 +0100

    Fixing some stuff

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from game.game import displayPositions,logic
 
 app = Flask(__name__)
 
@@ -18,37 +17,7 @@ def PVE():
 
 @app.route('/PVP')
 def PVP():
-    def game():
-        board = [['-','-','-'],['-','-','-'],['-','-','-']]
-        win = False
-        winner = None
-        positions = [1,2,3,4,5,6,7,8,9]
-        turn = 'X'
-        count = 0
-
-        while not win:
-            user_choise = int(input('Select a position: '))
-            if user_choise in positions:
-                displayPositions(turn, user_choise)
-                count += 1
-                if turn == 'X':
-                    turn = 'O'
-                else:
-                    turn = 'X'
-                positions.remove(user_choise)
-                logic(board)
-
-                if win:
-                    print(f"{winner} is the winner")
-                    break
-            else:
-                print("Invalid Position")
-                continue
-            if count == 9:
-                print('Game draw')
-                break
-        return game()
-    return render_template('PVP/pvp.html', game=game())
+    return render_template('PVP/pvp.html')
 
 @app.route('/Multiplayer')
 def Multiplayer():
